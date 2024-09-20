b7242706c16051c10ecc0ff58bb7e4689cb19d48
jatin-threely <jatin@threely.io>
2024-09-20 22:43:32 +0530

    added solana

--- LIT/createDIDData.js
+++ LIT/createDIDData.js
@@ -12,12 +12,19 @@ const getCreateDIDData = async (triaName, pkpData, authMethod, litNodeClient) =>
       litNodeClient: litNodeClient,
       pkp: pkpData,
     });
-    const { pkpAddress, generatedPublicKey, id} = await generatePrivateKey({
+    const promise1 = generatePrivateKey({
       pkpSessionSigs: sessionSig,
       network: 'evm',
       litNodeClient: litNodeClient,
       memo: Math.floor(1000000000 + Math.random() * 9000000000).toString()
     });
+    const promise2 = generatePrivateKey({
+      pkpSessionSigs: sessionSig,
+      network: 'solana',
+      litNodeClient: litNodeClient,
+      memo: Math.floor(1000000000 + Math.random() * 9000000000).toString()
+    });
+    const [{id: evmId, generatedPublicKey, pkpAddress}, {id:solanaId}] = await Promise.all([promise1, promise2]);
     const wrappedEthAddress = await ethers.utils.computeAddress(generatedPublicKey);
     if(pkpAddress !== pkpData.ethAddress) {
       throw new Error('pkpAddress is not equal to ethAddress');
@@ -32,15 +39,23 @@ const getCreateDIDData = async (triaName, pkpData, authMethod, litNodeClient) =>
       //   JSON.stringify(evmMessage),
     //   authMethod
     // );
-    const signature = await signMessageWithEncryptedKey({
+    const evmsignaturePromise = signMessageWithEncryptedKey({
       pkpSessionSigs: sessionSig,
       network: 'evm',
       messageToSign:  JSON.stringify(evmMessage),
       litNodeClient: litNodeClient,
-      id: id
+      id: evmId
     });
-    console.log("signature", signature);
-    //const recoverAddress = ethers.utils.verifyMessage(signature, JSON.stringify(evmMessage));
+    const solanaSignaturePromise = signMessageWithEncryptedKey({
+      pkpSessionSigs: sessionSig,
+      network: 'solana',
+      messageToSign:  JSON.stringify(evmMessage),
+      litNodeClient: litNodeClient,
+      id: solanaId
+    });
+    const [evmsignature, solanaSignature] = await Promise.all([evmsignaturePromise, solanaSignaturePromise])
+    console.log("signature", evmsignature);
+    //const recoverAddress = ethers.utils.verifyMessage(evmsignature, JSON.stringify(evmMessage));
 
     // if(recoverAddress !== wrappedEthAddress) {
     //   throw new Error('recoverAddress is not equal to wrappedEthAddress');
@@ -48,7 +63,7 @@ const getCreateDIDData = async (triaName, pkpData, authMethod, litNodeClient) =>
     const evmChainData = {
       address: evmAddress,
       message: evmMessage,
-      signature,
+      signature:evmsignature,
     };
 
     const args = {
